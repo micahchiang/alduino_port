4b00794dc5b0eac92d2fa0cfa3c7fc09b0ca883f
Micah Chiang <mchiang@rentreporters.com>
2017-02-20 10:44:24 -0800

    elements aligned.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,23 +3,10 @@ import { Component, trigger, state, style, transition, animate, keyframes } from
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  animations: [
-    trigger('navigationInitiated', [
-      state('inactive', style({
-        opacity: 1
-      })),
-      state('active', style({
-        opacity:0
-      })),
-      transition('inactive  => active', animate('100ms ease-in')),
-      transition('active => inactive', animate('100ms ease-out'))
-    ]),
-  ]
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   counter = 1;
-  state: string = 'inactive';
   showInfo = false;
   firstLoad = true;
   imageLeft = '/assets/imgs/'+this.counter+'_l.jpg';
@@ -27,35 +14,23 @@ export class AppComponent {
   imageRight = '/assets/imgs/'+this.counter+'_r.jpg'; 
 
   next() {
-    this.state = 'active';
     this.counter++;
     if(this.counter == 22){
       this.counter = 1;
     }
-    setTimeout(()=>{
-      this.imageLeft = '/assets/imgs/'+this.counter+'_l.jpg';
-      this.imageCenter = '/assets/imgs/'+this.counter+'_c.jpg';
-      this.imageRight = '/assets/imgs/'+this.counter+'_r.jpg';
-    },500);
-    setTimeout(()=>{
-      this.state = 'inactive';
-    }, 500);
+    this.imageLeft = '/assets/imgs/'+this.counter+'_l.jpg';
+    this.imageCenter = '/assets/imgs/'+this.counter+'_c.jpg';
+    this.imageRight = '/assets/imgs/'+this.counter+'_r.jpg';
   }
 
   previous() {
-    this.state = 'active';
     this.counter--;
     if(this.counter == 0){
       this.counter = 21;
     }
-    setTimeout(()=>{
-          this.imageLeft = '/assets/imgs/'+this.counter+'_l.jpg';
-          this.imageCenter = '/assets/imgs/'+this.counter+'_c.jpg';
-          this.imageRight = '/assets/imgs/'+this.counter+'_r.jpg';
-        },500);
-        setTimeout(()=>{
-          this.state = 'inactive';
-        }, 500);
+      this.imageLeft = '/assets/imgs/'+this.counter+'_l.jpg';
+      this.imageCenter = '/assets/imgs/'+this.counter+'_c.jpg';
+      this.imageRight = '/assets/imgs/'+this.counter+'_r.jpg';
     }
 
     toggleInfo() {
