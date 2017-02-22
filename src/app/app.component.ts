79e591b9bfa6b62928f843fa306e42264fd4890d
Micah Chiang <mchiang@rentreporters.com>
2017-02-22 10:34:43 -0800

    added distribution support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,18 +9,18 @@ export class AppComponent {
   counter = 1;
   showInfo = false;
   firstLoad = true;
-  imageLeft = '/assets/imgs/'+this.counter+'_l.jpg';
-  imageCenter = '/assets/imgs/'+this.counter+'_c.jpg';
-  imageRight = '/assets/imgs/'+this.counter+'_r.jpg'; 
+  imageLeft = 'assets/imgs/'+this.counter+'_l.jpg';
+  imageCenter = 'assets/imgs/'+this.counter+'_c.jpg';
+  imageRight = 'assets/imgs/'+this.counter+'_r.jpg'; 
 
   next() {
     this.counter++;
     if(this.counter == 22){
       this.counter = 1;
     }
-    this.imageLeft = '/assets/imgs/'+this.counter+'_l.jpg';
-    this.imageCenter = '/assets/imgs/'+this.counter+'_c.jpg';
-    this.imageRight = '/assets/imgs/'+this.counter+'_r.jpg';
+    this.imageLeft = 'assets/imgs/'+this.counter+'_l.jpg';
+    this.imageCenter = 'assets/imgs/'+this.counter+'_c.jpg';
+    this.imageRight = 'assets/imgs/'+this.counter+'_r.jpg';
   }
 
   previous() {
@@ -28,9 +28,9 @@ export class AppComponent {
     if(this.counter == 0){
       this.counter = 21;
     }
-      this.imageLeft = '/assets/imgs/'+this.counter+'_l.jpg';
-      this.imageCenter = '/assets/imgs/'+this.counter+'_c.jpg';
-      this.imageRight = '/assets/imgs/'+this.counter+'_r.jpg';
+      this.imageLeft = 'assets/imgs/'+this.counter+'_l.jpg';
+      this.imageCenter = 'assets/imgs/'+this.counter+'_c.jpg';
+      this.imageRight = 'assets/imgs/'+this.counter+'_r.jpg';
     }
 
     toggleInfo() {
